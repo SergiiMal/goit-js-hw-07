9607cba8b28dc845922143ecda8fcbdd49d42ab6
SergiiMal <ser4ester@gmail.com>
2023-01-19 10:48:23 +0200

    funct

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,4 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
-console.log(galleryItems);
+// const galleryElem = document.querySelector('.gallery');
+
+
+console.log(createNewGallery(images));
+ 
+function createNewGallery(images) {
+	return images
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
+  						<a class="gallery__link" href="${original}">
+    					<img class="gallery__image"
+						src="${preview}"
+						data-source="${original}"
+						alt="${description}"
+							/>
+						</a>
+						</div>`;
+	}).join("");	
+}
+
