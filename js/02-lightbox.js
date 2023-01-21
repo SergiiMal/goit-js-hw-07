e7745bb307690e32fa27e8f46c4d9a2098d53957
SergiiMal <ser4ester@gmail.com>
2023-01-21 17:26:04 +0200

    dz7/2_complit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,16 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
  const galleryElem = document.querySelector('.gallery');
  const galleryInsertHtml = createNewGallery(galleryItems);
 
  function createNewGallery(galleryItems){
-	return galleryItems
-	.map(({preview, original, description}) => {
-		return `
+	
+	 return galleryItems
+		 .map(({ preview, original, description }) => {
+			 return `
 		<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`
-	}).join('')
+</a>`;
+		 }).join('');
+	 
  }
+galleryElem.innerHTML = galleryInsertHtml;
+
+galleryElem.addEventListener('click', openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      simpleBox.close();
+    }
+  }  
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
