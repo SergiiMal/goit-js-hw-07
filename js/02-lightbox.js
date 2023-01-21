4739e544ed2d473960a4f85e3d8c3de4a7830010
SergiiMal <ser4ester@gmail.com>
2023-01-21 15:01:47 +0200

    add_<a

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+ const galleryElem = document.querySelector('.gallery');
+ const galleryInsertHtml = createNewGallery(galleryItems);
+
+ function createNewGallery(galleryItems){
+	return galleryItems
+	.map(({preview, original, description}) => {
+		return `
+		<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+	}).join('')
+ }
